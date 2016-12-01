cdbdd195d2ac15119a1851402b6a5b109d3e0395
Sean McNally <seanmcnally88@gmail.com>
2016-11-30 20:50:10 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+
+var AuthentifyManager = (function(){
+
+  function toggleSearch() {
+    $('.menu ul li:last-child').on('click', function(){
+      $('#search-bar').toggleClass('show-me');
+    });
+  }
+
+  function toggleSubmenu() {
+    $('.menu ul li:not(:nth-last-child(-n + 2))').on('click', function(){
+      $('#submenu').toggleClass('show-me');
+    });
+  }
+
+  function toggleHero(){
+    $('.change-hero').on('click', function(){
+      $('#hero1content').toggleClass('hide-me');
+      $('#hero2content').toggleClass('show-me');
+    });
+  }
+  function toggleMobileNav() {
+    $('#nav-icon3').click(function(){
+      $(this).toggleClass('open');
+      if ($('.mobile-menu').css('display') == 'none') {
+        $('.mobile-menu').show();
+      } else {
+        $('.mobile-menu').hide();
+      }
+    });
+  }
+  function toggleMobileSubmenu() {
+    $('.mobile-menu ul li:not(:nth-last-child(-n + 1))').on('click', function(){
+      $('#submenu').toggleClass('show-me');
+    });
+  }
+
+  var publicAPI = {
+    toggleSearch: toggleSearch,
+    toggleSubmenu: toggleSubmenu,
+    toggleHero: toggleHero,
+    toggleMobileNav: toggleMobileNav,
+    toggleMobileSubmenu: toggleMobileSubmenu
+  }
+
+  return publicAPI;
+})();
+
+$(document).ready(function(){
+  AuthentifyManager.toggleSearch(),
+  AuthentifyManager.toggleSubmenu(),
+  AuthentifyManager.toggleHero(),
+  AuthentifyManager.toggleMobileNav(),
+  AuthentifyManager.toggleMobileSubmenu()
+})
